ea995b6a27724dabdf296c4c9494bc23bfdf39b1
Nanyian <104672400+NAOXUAN4@users.noreply.github.com>
2025-08-07 16:04:23 +0800

    css@25/8/7

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 	import { onMounted, ref } from 'vue';
 	import WsButton from './components/Button/Button.vue';
-	import type { ButtonInstance } from './components/Button/type';
+	import type { ButtonInstance, ButtonType } from './components/Button/type';
 	const ButtonDom = ref<ButtonInstance>();
 
 	onMounted(()=>{
@@ -13,9 +13,14 @@
 <template>
 	<h1>PlayGround 😽</h1>
 	<div class = "playground">
-		<WsButton type="primary"  plain ref="ButtonDom">
-			button slot
-		</WsButton>
+		<div v-for="item in <ButtonType[]>['primary', 'error', 'warning', 'success', 'info']" style="margin: 2px;">
+			<WsButton :type=item>
+				{{ item }}
+			</WsButton>
+		</div>
+		<!-- <WsButton type="primary" ref="ButtonDom">
+			Primary
+		</WsButton> -->
 	</div>
 </template>
 
@@ -23,11 +28,9 @@
 	.playground{
     	padding: 2%;
     	border-radius: 15px; width: 90%; height: 400px;
-    	background: linear-gradient(to right, #bfdfe2, #9decc6);
-	}
-
-	.ws-button{
-		background-color: lightcoral;
+    	background: linear-gradient(to right, #214447, #000000);
+		display: flex;
+		flex-direction: row;
 	}
 	
 
--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -44,8 +44,6 @@
 
 
 <style scoped>
-    .ws-button{
-		background-color: var(--ws-bg-color);
-	}
+    @import './style.scss';
 
 </style>
